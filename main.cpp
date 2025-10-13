d44e6e78b5fcdf2c9e114ead33e3f9ca5285b497
Steve King <steve_king_1974@yahoo.co.uk>
2025-10-13 20:17:34 +0100

    Initial try

--- main.cpp
+++ main.cpp
@@ -65,23 +65,52 @@ int main(int argc, char *argv[])
     QLowEnergyAdvertisingData advertisingData;
     advertisingData.setDiscoverability(QLowEnergyAdvertisingData::DiscoverabilityGeneral);
     advertisingData.setIncludePowerLevel(true);
-    advertisingData.setLocalName("HeartRateServer");
-    advertisingData.setServices(QList<QBluetoothUuid>() << QBluetoothUuid::ServiceClassUuid::HeartRate);
+    advertisingData.setLocalName("PowerMeterServer");
+    advertisingData.setServices(QList<QBluetoothUuid>() << QBluetoothUuid::ServiceClassUuid::CyclingPower);
     //! [Advertising Data]
 
     //! [Service Data]
-    QLowEnergyCharacteristicData charData;
-    charData.setUuid(QBluetoothUuid::CharacteristicType::HeartRateMeasurement);
-    charData.setValue(QByteArray(2, 0));
-    charData.setProperties(QLowEnergyCharacteristic::Notify);
-    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration,
-                                                QByteArray(2, 0));
-    charData.addDescriptor(clientConfig);
-
+    // SensorLocation = 0x2a5d,
+    //     /* 0x2a5e not defined */
+    //     /* 0x2a5f not defined */
+    //     /* 0x2a60 not defined */
+    //     /* 0x2a61 not defined */
+    //     /* 0x2a62 not defined */
+    //     CyclingPowerMeasurement = 0x2a63,
+    //     CyclingPowerVector = 0x2a64,
+    //     CyclingPowerFeature = 0x2a65,
+    //     CyclingPowerControlPoint = 0x2a66,
     QLowEnergyServiceData serviceData;
     serviceData.setType(QLowEnergyServiceData::ServiceTypePrimary);
-    serviceData.setUuid(QBluetoothUuid::ServiceClassUuid::HeartRate);
-    serviceData.addCharacteristic(charData);
+    serviceData.setUuid(QBluetoothUuid::ServiceClassUuid::CyclingPower);
+
+    QLowEnergyCharacteristicData powerMeasurementCharData;
+    powerMeasurementCharData.setUuid(QBluetoothUuid::CharacteristicType::CyclingPowerMeasurement);
+    powerMeasurementCharData.setValue(QByteArray(4, 0));
+    powerMeasurementCharData.setProperties(QLowEnergyCharacteristic::Notify);
+    const QLowEnergyDescriptorData pmClientConfig(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration,
+                                                QByteArray(4, 0));
+    powerMeasurementCharData.addDescriptor(pmClientConfig);
+    serviceData.addCharacteristic(powerMeasurementCharData);
+
+    QLowEnergyCharacteristicData featureCharData;
+    featureCharData.setUuid(QBluetoothUuid::CharacteristicType::CyclingPowerFeature);
+    featureCharData.setValue(QByteArray(2, 0));
+    featureCharData.setProperties(QLowEnergyCharacteristic::Read);
+    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration,
+                                                QByteArray(2, 0));
+    featureCharData.addDescriptor(clientConfig);
+    serviceData.addCharacteristic(featureCharData);
+
+    QLowEnergyCharacteristicData sensorLocationCharData;
+    sensorLocationCharData.setUuid(QBluetoothUuid::CharacteristicType::SensorLocation);
+    sensorLocationCharData.setValue(QByteArray(1, 13));
+    sensorLocationCharData.setProperties(QLowEnergyCharacteristic::Read);
+    const QLowEnergyDescriptorData locClientConfig(QBluetoothUuid::DescriptorType::ClientCharacteristicConfiguration,
+                                                QByteArray(1, 13));
+    sensorLocationCharData.addDescriptor(locClientConfig);
+    serviceData.addCharacteristic(sensorLocationCharData);
+
     //! [Service Data]
 
     //! [Start Advertising]
@@ -106,28 +135,28 @@ int main(int argc, char *argv[])
     //! [Start Advertising]
 
     //! [Provide Heartbeat]
-    QTimer heartbeatTimer;
-    quint8 currentHeartRate = 60;
+    QTimer powermeterTimer;
+    qint16 currentPower = 60;
     enum ValueChange { ValueUp, ValueDown } valueChange = ValueUp;
-    const auto heartbeatProvider = [&service, &currentHeartRate, &valueChange]() {
+    const auto powerProvider = [&service, &currentPower, &valueChange]() {
         QByteArray value;
-        value.append(char(0)); // Flags that specify the format of the value.
-        value.append(char(currentHeartRate)); // Actual value.
+        QDataStream s(&value, QIODeviceBase::WriteOnly);
+        s << quint16(0) << currentPower;
         QLowEnergyCharacteristic characteristic
-            = service->characteristic(QBluetoothUuid::CharacteristicType::HeartRateMeasurement);
+            = service->characteristic(QBluetoothUuid::CharacteristicType::CyclingPowerMeasurement);
         Q_ASSERT(characteristic.isValid());
         service->writeCharacteristic(characteristic, value); // Potentially causes notification.
-        if (currentHeartRate == 60)
+        if (currentPower == 60)
             valueChange = ValueUp;
-        else if (currentHeartRate == 100)
+        else if (currentPower == 100)
             valueChange = ValueDown;
         if (valueChange == ValueUp)
-            ++currentHeartRate;
+            ++currentPower;
         else
-            --currentHeartRate;
+            --currentPower;
     };
-    QObject::connect(&heartbeatTimer, &QTimer::timeout, heartbeatProvider);
-    heartbeatTimer.start(1000);
+    QObject::connect(&powermeterTimer, &QTimer::timeout, powerProvider);
+    powermeterTimer.start(1000);
     //! [Provide Heartbeat]
 
     auto reconnect = [&leController, advertisingData, &service, serviceData]() {
